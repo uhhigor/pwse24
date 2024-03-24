0e18c658c480d8fa0935e6980597a6cb86ac6412
MaciejPerdaszek <maciej.44@wp.pl>
2024-03-24 17:32:14 +0100

    Frontend tasks init

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from "./auth/LoginPage";
 import RegisterPage from "./auth/RegisterPage";
+import TaskPage from './tasks/TaskPage';
 import axios from "axios";
+
 axios.defaults.withCredentials = true;
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <LoginPage />
-          <RegisterPage />
-      </header>
+      <Router>
+        <header className="App-header">
+          <Routes>
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/tasks" element={<TaskPage />} />
+          </Routes>
+        </header>
+      </Router>
     </div>
   );
 }
+
 
 export default App;
