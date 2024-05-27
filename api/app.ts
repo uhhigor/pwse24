f8a465713cb5e3e6ab8891cc20aef8e1a334b444
jkszymczak <kuba_szymcz@wp.pl>
2024-05-27 21:38:33 +0200

    fixing cors

--- api/app.ts
+++ api/app.ts
@@ -21,7 +21,7 @@ let testerRouter = require('./routes/tester')
 
 const app = express();
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'http://localhost:5005',
     credentials: true
 }));
 app.use(logger('dev'));
